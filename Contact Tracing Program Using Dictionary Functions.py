7d1db476794029532f05263b49d2cda135aca3d1
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2022-11-12 19:26:29 +0800

    Allows user input

--- Contact Tracing Program Using Dictionary Functions.py
+++ Contact Tracing Program Using Dictionary Functions.py
@@ -12,5 +12,21 @@ def print_menu():
 
 print_menu()
 # Allows user input to select item in the menu
+try:
+    user_input = int(input("What do you want to do: "))
 # Perform the selected option
+except ValueError:
+    print("Invalid choice.")
+else:
+    if user_input == 1:
+        full_name = input("Full name: ")
+        gender = input("Gender [Male/Female/Other]: ")
+        age = int(input("Age: "))
+        address = input("Address: ")
+        email_address = input("Email Address: ")
+        phone_number = int(input("Phone number: "))
+        vaccination_status = input("Vaccination Status [Not Yet/1st Dose/2nd Dose]: ")
+        covid_status = input("Positive for COVID in the last 14 days [Y/N]: ")
+
+
 
