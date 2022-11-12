d9aa78d7858d88d4ef49af1cbaa4fefab507545f
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2022-11-12 19:10:28 +0800

    Display a menu of options

---
+++ Contact Tracing Program Using Dictionary Functions.py
@@ -0,0 +1,16 @@
+# Programmed by Erika Jane T. Reyes
+# Python program for contact tracing using dictionary functions
+
+# Display a menu of options
+def print_menu():
+    print("*" * 38)
+    print("\tContact Tracing Program")
+    print("\n" + "*"* 15 + "  MENU  " + "*" * 15)
+    print("1 -> Add an item")
+    print("2 -> Search")
+    print("3 -> Exit program [Y/N]")
+
+print_menu()
+# Allows user input to select item in the menu
+# Perform the selected option
+
